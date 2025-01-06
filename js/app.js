3e5dfed663f2f9d22ed7e9a912cdf0dc1ef5c0b5
merliariza <180667185+merliariza@users.noreply.github.com>
2025-01-06 10:40:09 -0500

    feat: :zap: Cover to index page
    Js funcional

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const splashVideo = document.getElementById('splashVideo');
+const startButton = document.getElementById('startButton');
+const splashScreen = document.querySelector('.splash-screen');
+const mainContent = document.querySelector('.main-content');
+
+// Pausa el video cuando termine
+splashVideo.addEventListener('ended', () => {
+  splashVideo.pause();
+  splashVideo.currentTime = splashVideo.duration - 0.1; 
+});
+
+// Acción cuando se presiona el botón de "Start"
+startButton.addEventListener('click', () => {
+  // Desaparece la pantalla de splash
+  splashScreen.style.opacity = '0';
+
+  // Permite el desplazamiento 
+  document.body.style.overflow = 'auto';
+
+  // Muestra la página principal
+  setTimeout(() => {
+    splashScreen.style.display = 'none';
+    mainContent.style.display = 'block';
+  }, 500); 
+});
